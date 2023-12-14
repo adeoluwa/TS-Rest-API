fad59ba88ccd396c3c2c996a1a5b3140b51fb204
DeoluJoseph <kiishijoseph@gmail.com>
2023-12-14 17:18:01 +0100

    implemented delete account function

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import { deleteUser, getAllUsers } from '../controllers/users';
-import { isAuthenticated } from '../middlewares';
+import { isAuthenticated, isOwner } from '../middlewares';
 
 export default (router: express.Router) => {
     router.get('/users', isAuthenticated, getAllUsers);
-    router.delete('/users/:id', deleteUser)
+
+    /** a user deleting thier own account, they have to be logged in to be able to carry out the delete account ops */
+    router.delete('/users/:id',isAuthenticated,isOwner ,deleteUser) 
 };
