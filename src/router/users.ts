68dee25473611dbe908edcade3bb589574e294ad
DeoluJoseph <kiishijoseph@gmail.com>
2023-12-14 17:38:52 +0100

    fixed updateUser's bug

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
-import { deleteUser, getAllUsers } from '../controllers/users';
+import { deleteUser, getAllUsers, updateUser } from '../controllers/users';
 import { isAuthenticated, isOwner } from '../middlewares';
 
 export default (router: express.Router) => {
     router.get('/users', isAuthenticated, getAllUsers);
 
     /** a user deleting thier own account, they have to be logged in to be able to carry out the delete account ops */
-    router.delete('/users/:id',isAuthenticated,isOwner ,deleteUser) 
+    router.delete('/users/:id',isAuthenticated,isOwner ,deleteUser);
+
+    router.patch('/users/:id', isAuthenticated, isOwner, updateUser);
 };
