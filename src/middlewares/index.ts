08480a0d37cdf5526f2668e8af0253636ace2748
DeoluJoseph <kiishijoseph@gmail.com>
2023-12-14 16:56:43 +0100

    implemented user authentication routes, users routes, authentication middleware, and authentication helper functions

---
+++ src/middlewares/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import {get, merge} from 'lodash'
+
+import { getUserBySessionToken } from '../db/users';
+
+export const isAuthenticated = async(req: express.Request,res: express.Response, next: express.NextFunction) => {
+    try {
+        const sessionToken = req.cookies['ADEOLU-AUTH'];
+
+        if(!sessionToken){
+            return res.sendStatus(403);
+        }
+
+        const exisitingUser = await getUserBySessionToken(sessionToken);
+
+        if(!exisitingUser){
+            return res.sendStatus(403); 
+        }
+
+        merge(req, { identity: exisitingUser });
+
+        return next();
+
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    };
+};
