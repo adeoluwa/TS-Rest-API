417231310569c148e918d18bc88ef0719c71a037
DeoluJoseph <kiishijoseph@gmail.com>
2023-12-20 19:19:34 +0100

    removed store services for testing

---
+++ src/router/store.ts
@@ -0,0 +1,16 @@
+import express from "express"
+
+// import { addProduct, updateProductById, deleteProduct, getAllProducts, getProductById  } from "../services/store-services"
+
+export default (router: express.Router) => {
+//     router.post('/store', addProduct);
+
+//     router.patch('/store/:id', updateProductById);
+
+//     router.get('/store', getAllProducts);
+
+//     router.get('/store/:id', getProductById);
+
+//     router.delete('store/:id', deleteProduct);
+//
+ }
